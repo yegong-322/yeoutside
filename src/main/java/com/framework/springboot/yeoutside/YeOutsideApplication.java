d153cb2bcde80057473cc96f9c8ff9944b0b72bc
yal <yal@renocomms.com>
2024-03-06 11:48:19 +0900

    first commit

---
+++ src/main/java/com/framework/springboot/yeoutside/YeOutsideApplication.java
@@ -0,0 +1,13 @@
+package com.framework.springboot.yeoutside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeOutsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeOutsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/framework/springboot/yeoutside/YeOutsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.springboot.yeoutside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeOutsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
